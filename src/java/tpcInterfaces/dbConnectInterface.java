cd65afd048153277ef4975a36851530be40ad464
Devito <devito@devitocorp.com>
2022-01-03 10:33:39 +0530

    First Commit

---
+++ src/java/tpcInterfaces/dbConnectInterface.java
@@ -0,0 +1,8 @@
+package tpcInterfaces;
+
+public class dbConnectInterface {
+    String hostname = "localhost:3306";
+    String dbname = "theplacementcell";
+    String username = "root";
+    String password = "devito";
+}
